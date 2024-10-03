9a675811a08d57fb9e9cf0e53d9daf30c56c8bb6
Krupali2626 <Krupalikumbhani26@gmail.com>
2024-10-03 10:50:12 +0530

    "morning"

--- src/App.js
+++ src/App.js
@@ -12,52 +12,20 @@ import './CSS/carousel.css'
 import './CSS/Accordian.css'
 import './CSS/Footer.css'
 import './CSS/d_navbar.css'
-import Index from './Container/Index';
-import CustomNavbar from './Componant/CustomNavbar';
-import SignIn from './Componant/SignIn';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store';
-import ValForm from './ValForm';
-import Footer from './Componant/Footer';
-import Feature from './Container/Feature';
-import AboutUs from './Container/AboutUs';
-import Pricing from './Container/Pricing';
-import Help from './Container/Help';
-import Contect_us from './Container/Contect_us'
-
-import Sidebar from './Editing/Sidebar';
 import DenishaRoutes from './Editing/Denisha.routes';
-import PrivacyPolicy from './Container/PrivacyPolicy';
-import TndC from './Container/TndC';
-import Payment from './Container/Payment';
+import Krupalirouter from './Routing/Krupalirouter';
 function App() {
   return (
     <>
 
       {/* <CustomNavbar />  */}
       <Provider store={store}>
-      <BrowserRouter>
-        <DenishaRoutes /> {/* Include the routing logic */}
-      </BrowserRouter>
-        {/* <Routes> */}
-        {/* <Sidebar /> */}
-          {/* <Route path="/signin" element={<SignIn />} />
-          <Route path="/" element={<Index />} />
-          <Route path="/feature" element={<Feature />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/contact" element={<Contect_us />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<TndC />} />
-          <Route path="/pricing/payment" element={<Payment />} />
-          <Route path="/footer" element={<Footer />} />
-          <Route path="/Sidebar" element={<Sidebar />} /> */}
-           {/* <Route path="/form" element={<ValForm />} />  */}
-        {/* </Routes> */}
+        <Krupalirouter />
+        <DenishaRoutes />
+        
       </Provider>
-      {/* <Footer /> */}
     </>
   );
 }
